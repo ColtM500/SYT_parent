5dc4c0c15c92324a50b5e18c97f78576fd36ee2e
chen <602042954@qq.com>
2023-06-12 08:55:00 +0800

    Initial commit

---
+++ common/common_util/src/test/java/cn/chen/common_util/CommonUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chen.common_util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ common/service_util/src/test/java/cn/chen/service_util/ServiceUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chen.service_util;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
